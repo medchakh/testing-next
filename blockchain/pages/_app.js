349063ad875926a5b2618c93d539c7202994daa5
medchakh <medchakhman@gmail.com>
2022-11-16 21:47:42 +0000

    save changes

--- blockchain/pages/_app.js
+++ blockchain/pages/_app.js
@@ -1,7 +1,8 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThirdwebProvider  desiredChainId={ChainId.Mainnet}> <Component {...pageProps} /></ThirdwebProvider>
 }
 
 export default MyApp
