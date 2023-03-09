dd7f2861fa6ef4ac94e013be3a61f6ed5f6f4cfd
medchakh <medchakhman@gmail.com>
2023-03-09 14:27:11 +0000

    m

--- blockchain/pages/index.js
+++ blockchain/pages/index.js
@@ -1,46 +1,23 @@
+import {  useState } from "react";
+import styles from "../styles/Home.module.css";
+import { useWalletConnect, useCoinbaseWallet, useMetamask } from "@thirdweb-dev/react";
 
-import { useAddress, useContract, ConnectWallet } from '@thirdweb-dev/react'
-import { useEffect, useState } from 'react'
-import Listing from '../components/Listing'
-import styles from '../styles/Home.module.css'
+
 
 function Home() {
-  const userAddress = useAddress()
-  const { contract, isLoading } = useContract("0xEde2c80b714A41c513431599D787Acb230Ab215C", "marketplace")
- const [listings, setListings] = useState() 
- async function buyOutToken(listingId){
-  if(!isLoading){
-    try{
-      await contract?.buyoutListing(listingId, 1)
-      console.log('success buying')
-    }catch(err){
-      alert('cannot buy token', err)
-    } 
-  }
- }
- useEffect(()=> {
-  async function getListings(){
-    try{
-      if(!isLoading){
-        const getlistings = await contract?.getAllListings()
-        setListings(getlistings)
-        console.log(listings)
-      }
-    }catch(err){
-      return alert('didnt work')
-    }
-  }
-  getListings()
- })
+  //   const { contract, isLoading } = useContract("0xEde2c80b714A41c513431599D787Acb230Ab215C", "marketplace")
+  const [listings, setListings] = useState();
+  const connectCoinbase = useCoinbaseWallet();
+  const connectWithWalletConnect = useWalletConnect();
+  const connectWithMetamask = useMetamask();
+
   return (
     <div className={styles.container}>
-            <ConnectWallet />
-
-      {listings?.map(listing => {return <Listing name={listing.asset.name} image={listing.asset.image} 
-      buyOutPrice={(listing?.currencyContractAddress / 1e81)} buyToken={() => buyOutToken(listing?.asset?.id)}/>})}
-      </div>
-  )
+      <div className={styles.item} onClick={() => connectWithWalletConnect()}>Wallet Connect</div>
+      <div className={styles.item} onClick={() => connectCoinbase()}>connect Coinbase</div>
+      <div className={styles.item} onClick={() => connectWithMetamask()}>connect With Metamask</div>
+    </div>
+  );
 }
 
-export default Home
-
+export default Home;
