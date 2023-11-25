0449829a47ce5caba665fbdd8a09325e7494bde0
medchakh <medchakhman@gmail.com>
2023-11-25 18:27:52 +0000

    npx

--- blockchain/pages/_app.js
+++
@@ -1,9 +0,0 @@
-
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <ThirdwebProvider  desiredChainId={ChainId.Mumbai}> <Component {...pageProps} /></ThirdwebProvider>
-}
-
-export default MyApp
